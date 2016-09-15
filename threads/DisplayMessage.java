c7b68166c4d665155e2e2e65e8b0644f9bd3f14d
Justin S. Baiko <baiko@utexas.edu>
2016-09-15 12:01:33 -0500

    multi-threading examples

---
+++ threads/DisplayMessage.java
@@ -0,0 +1,16 @@
+// Create a thread to implement Runnable
+public class DisplayMessage implements Runnable
+{
+   private String message;
+   public DisplayMessage(String message)
+   {
+      this.message = message;
+   }
+   public void run()
+   {
+      while(true)
+      {
+         System.out.println(message);
+      }
+   }
+}
